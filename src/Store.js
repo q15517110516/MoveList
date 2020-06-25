f9f3c9f820d9c9d210879d196c0a8cf1e781f021
q15517110516 <lmrzds@gmail.com>
2020-06-25 16:44:11 -0400

    create store

--- src/Store.js
+++ src/Store.js
@@ -2,11 +2,8 @@ import { createStore } from 'redux';
 import reducer from './Reducers/Reducer';
 
 
-const initialState = {};
+// const initialState = {};
 
-const store = createStore(
-    reducer,
-    initialState
-)
+const store = createStore(reducer)
 
 export default store;
