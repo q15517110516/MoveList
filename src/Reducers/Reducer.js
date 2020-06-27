c59a3b1a1fdc27c7ec37e6114462e1fd3fa9ff17
q15517110516 <lmrzds@gmail.com>
2020-06-26 20:36:47 -0400

    add to favorite action in reducer

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -1,11 +1,31 @@
 import Movies from '../Movies-img/Movies';
+import { ADD_TO_FAVORITES } from '../Actions/ActionTypes'
 
 
 const initialState = {
     movies: Movies,
-    addedFavorite: []
+    favorite: []
 }
 
 export default function(state = initialState, action){
-    return state;
+    let addedMovie = state.movies.find(movie => movie.id === action.id);
+    let existedMovie = state.favorite.find(movie => action.id === movie.id);
+    switch(action.type){
+        case ADD_TO_FAVORITES:
+            if(existedMovie){
+                return {
+                    favorite: [addedMovie]
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    favorite: [...state.favorite, addedMovie]
+                }
+            };
+
+        default:
+            return state;
+    }
+    
 }
