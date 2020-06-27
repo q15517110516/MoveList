2df89a49e450309eb0ff77ddaf05d3c0b2c6f817
q15517110516 <lmrzds@gmail.com>
2020-06-26 21:03:25 -0400

    1st draft 'add to favorite' feature

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "antd/dist/antd.css";
+import { connect } from 'react-redux';
+import { Typography, Button } from "antd";
+import '../Movie.css';
+
+const { Title } = Typography;
 
 export class Favorites extends Component {
-    constructor(){
-        super();
-
-    }
+    
     render() {
-        const movie = this.props.movie;
-        console.log(movie)
+        
+        
+        let addedMovies = this.props.favorite.length ? (
+            this.props.favorite.map(movie => {
+                return (
+                    <div className="posters" key={movie.id}>
+                        <img className="movie-img" id={movie.id} src={movie.img} alt="posters"width={238} height={340}/>
+                        <div className="movie-title">
+                            <Title level={4} style={{color: "white"}}>{movie.name}</Title>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <Title level={1} style={{color: 'white'}}>You Don't Have Favorite Movies</Title>
+        )
         return (
-            <div>
-                
+            <div className="movies">
+                {addedMovies}
             </div>
         )
     }
 }
 
-export default Favorites
+const mapStateToProps = (state) => {
+    return {
+        favorite: state.favorite
+    }
+}
+
+export default connect(mapStateToProps)(Favorites)
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-// import Movies from '../Movies-img/Movies';
 import { Typography, Button } from "antd";
 import "antd/dist/antd.css";
-import './Home.css';
-import $ from 'jquery';
-import Favorites from '../Favorites/Favorites';
+import '../Movie.css';
 import { connect } from 'react-redux';
+import { addToFavorites } from '../Actions/Actions';
+
 
 const { Title } = Typography;
 
@@ -13,93 +12,50 @@ export class Home extends Component {
     constructor(){
         super();
         this.state={
-            favorite: [],
-            // selectMovie: Movies[0],
             disabled: false,
             btnLabel: "Add"
         }
     }
 
 
-    changeMovie = (movie) => {
-        this.setState({
-            // selectMovie: movie
-        });
-        // console.log(movie);
-    }
-
-    addToFavorites = (movie) => {
-        let favorites = this.state.favorite;
-        let movieID = movie.id;
-
-        // this.setState({
-        //     btnLabel: 'Already Added'
-        // })
-
-        // if selected movie already added in favorite
-        if(this.checkMovie(movieID)){
-            console.log('already added');
-            // let index = favorites.findIndex(x => x.id == movieID);
-        }
-        //if not, add the movie in favorite
-        else{
-            favorites.push(movie);
-        }
-
-        this.setState({
-            favorite: favorites,
-        });
-
-        console.log(this.state.favorite);
-
-    }
-
-    // check if selected movie is already in favorites
-    checkMovie = (movieID) => {
-        let favorites = this.state.favorite;
-        return favorites.some(function(item){
-            return item.id === movieID;
-        })
-    }
-
-    componentDidMount = () => {
-        // $(function(){
-        //     $(".movie-img").mouseover(function(){          
-        //         $(".mask").show();
-                
-        //     });
-        //     $(".movie-img").mouseout(function(){
-        //         $(".mask").hide();
-        //     });
-        // });
-        
+    handleClick = (id) => {
+        this.props.addToFavorites(id)
     }
 
 
     render() {
-        console.log(this.props.movies)
+        
+        let movieList = this.props.movies.map(movie => {
+            return (
+                <div className="posters" key={movie.id}>
+                    <img className="movie-img" id={movie.id} src={movie.img} alt="posters"width={238} height={340}/>
+                    <div className="movie-title">
+                        <Title level={4} style={{color: "white"}}>{movie.name}</Title>
+                        <Button type="primary" onClick={() => this.handleClick(movie.id)}>{this.state.btnLabel}</Button>
+                    </div>
+                </div>
+            )
+        })
         return (
             <div className="movies">
-                {this.props.movies.map(movie => (
-                    <div className="posters" key={movie.id}>
-                        <img className="movie-img" id={movie.id} src={movie.img} alt="posters"width={238} height={340} onMouseOver={() => this.changeMovie(movie)}/>
-                        <div className="movie-title">
-                            <Title level={4} style={{color: "white"}}>{movie.name}</Title>
-                            <Button type="primary" disabled={this.state.disabled} onClick={() => this.addToFavorites(movie)}>{this.state.btnLabel}</Button>
-                        </div>
-                    </div>
-                ))}
-
+                {movieList}
             </div>
             
         )
     }
 }
 
+// takes the state in our reducer and pass it as props in our file
 const mapStateToProps = (state) => {
     return {
         movies: state.movies
     }
 }
 
-export default connect(mapStateToProps)(Home)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addToFavorites: (id) => {dispatch(addToFavorites(id))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
