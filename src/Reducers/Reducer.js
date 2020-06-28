74642433e5e6cb9bc1b2a654d6c0b9107d34802f
q15517110516 <lmrzds@gmail.com>
2020-06-27 21:15:47 -0400

    add 'add to cart' action in reducer

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -1,15 +1,20 @@
-import Movies from '../Movies-img/Movies';
-import { ADD_TO_FAVORITES } from '../Actions/ActionTypes'
+import Movies from '../AllMovies';
+import { ADD_TO_FAVORITES, ADD_TO_CART } from '../Actions/ActionTypes'
 
 
 const initialState = {
     movies: Movies,
-    favorite: []
+    favorite: [],
+    cart: [],
+    totalPrice: 0
+
 }
 
 export default function(state = initialState, action){
     let addedMovie = state.movies.find(movie => movie.id === action.id);
     let existedMovie = state.favorite.find(movie => action.id === movie.id);
+    let cartMovie = state.cart.find(movie => action.id === movie.id);
+    
     switch(action.type){
         case ADD_TO_FAVORITES:
             if(existedMovie){
@@ -24,7 +29,19 @@ export default function(state = initialState, action){
                     favorite: [...state.favorite, addedMovie]
                 }
             };
-
+        case ADD_TO_CART:
+            if(cartMovie){
+                return {
+                    ...state,
+                    cart: [...state.cart]
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    cart: [...state.cart, addedMovie]
+                }
+            };
         default:
             return state;
     }
