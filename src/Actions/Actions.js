c3d83e68469b9f425cd37eaf8760bcffbb2cf806
q15517110516 <lmrzds@gmail.com>
2020-06-27 21:03:51 -0400

    add 'add to cart' action

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -1,8 +1,17 @@
 import { ADD_TO_FAVORITES } from './ActionTypes';
+import { ADD_TO_CART } from './ActionTypes';
+
 
 export const addToFavorites = (id) => {
     return {
         type: ADD_TO_FAVORITES,
+        id
+    };
+};
+
+export const addToCart = (id) => {
+    return {
+        type: ADD_TO_CART,
         id
     };
 };
