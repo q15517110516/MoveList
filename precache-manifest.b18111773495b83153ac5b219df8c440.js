8ab0cf3366a1860e37c6d9e56a7e113ef1b03b4c
q15517110516 <lmrzds@gmail.com>
2020-07-01 21:10:22 -0400

    Updates

---
+++ precache-manifest.b18111773495b83153ac5b219df8c440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a682973c860dd7e788b3716fb2134597",
+    "url": "/MovieList/index.html"
+  },
+  {
+    "revision": "25c184ff82ed018eb938",
+    "url": "/MovieList/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "9bd1069e877d834127f1",
+    "url": "/MovieList/static/css/main.9a0120c1.chunk.css"
+  },
+  {
+    "revision": "25c184ff82ed018eb938",
+    "url": "/MovieList/static/js/2.a093a570.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MovieList/static/js/2.a093a570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd1069e877d834127f1",
+    "url": "/MovieList/static/js/main.b71b6838.chunk.js"
+  },
+  {
+    "revision": "a6a957a7538f140db6dc",
+    "url": "/MovieList/static/js/runtime-main.49cb6e00.js"
+  }
+]);
